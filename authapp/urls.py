b762f3c169b1074241b7105c6f1470dbaea86414
Igor <gormal98@gmail.com>
2020-12-23 15:11:39 +0300

    lesson5

--- authapp/urls.py
+++ authapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'), 
     path('register/', auth_views.register, name='register'),
     path('logout/', auth_views.logout, name='logout'),
+    path('profile/', auth_views.profile, name='profile'),
 ]
