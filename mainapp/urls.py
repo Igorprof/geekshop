dcacf906e6474fa60f6a2abf4759e1c7682e6aee
Igor <gormal98@gmail.com>
2020-12-30 15:20:19 +0300

    lesson8

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp_views.products, name='products'),
+    path('<int:category_id>/', mainapp_views.products, name='product'),
+    path('page/<int:page>/', mainapp_views.products, name='page'),
 ]
